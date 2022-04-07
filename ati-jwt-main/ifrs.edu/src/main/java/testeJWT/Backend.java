56b0650264789127679be1a492247895e61b2601
Usuário <Usurio@LAB204-DDD65F.ad.poa.ifrs.edu.br>
2022-04-07 11:48:53 -0300

    commit ajustes finais

--- ati-jwt-main/ifrs.edu/src/main/java/testeJWT/Backend.java
+++ ati-jwt-main/ifrs.edu/src/main/java/testeJWT/Backend.java
@@ -5,8 +5,10 @@ import java.util.logging.Logger;
 
 import javax.annotation.security.RolesAllowed;
 import javax.inject.Inject;
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
@@ -25,9 +27,10 @@ public class Backend {
 
     @GET
     @Path("/{a}/{b}")
+    @Consumes(MediaType.TEXT_PLAIN)
     @Produces(MediaType.TEXT_PLAIN)
     @RolesAllowed({ "Admin" })
-    public int getSum(int a, int b){
+    public int getSum(@PathParam("a") int a, @PathParam("b") int b){
         LOGGER.log(Level.INFO, "Backend: {0}", email);
         return a + b;
     }
